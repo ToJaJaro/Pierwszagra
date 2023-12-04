0227f89ccbffb0eda4b7e920a633b23e3901ccae
ToJaJaro <karoljarosz51@gmail.com>
2023-12-04 22:15:07 +0100

    jakieś animacje zrobione
    trzeba zmienić żeby nie trzeba było przytrzymywać ataku, do tego będzie mocniejszy atak

--- Player_movement.gd
+++ Player_movement.gd
@@ -1,40 +1,54 @@
 extends CharacterBody2D
-
+#movement
 var player_speed = 400
 var dash_speed = 2.5 * player_speed
 var is_dashing = false
-var dash_duration = 0.2  # czas trwania dasha w sekundach
-var dash_timer = 0.0
+#var is_attacking = false
+var is_blocking = false
+#var is_walking = false
+#textury 
+#var player : Texture
+#var player_block : Texture
+#statystyki
+#var hp = 100
+#var strenght = 1
+#var posture = 100
+@onready var animation_tree = $AnimationTree
 
-var player : Texture
-var player_block : Texture
+func _ready():
+	animation_tree.active = true
 
-
-func _process(delta):
-	if Input.is_action_pressed("right_click"):
-		$player_block.visible = true  # Aktywuj Sprite gracza w bloku
-		$player.visible = false  # Ukryj Sprite gracza bazowego
-	else:
-		$player_block.visible = false  # Ukryj Sprite gracza w bloku
-		$player.visible = true  # Aktywuj Sprite gracza bazowego
+func _physics_process(_delta):
 	
-	if is_dashing:
-		dash_timer -= delta
-		if dash_timer <= 0:
-			is_dashing = false
-			player_speed = 400  # przywróć normalną prędkość gracza
+	if Input.is_action_pressed("right_click"):
+		animation_tree["parameters/conditions/is_blocking"] = true
+		animation_tree["parameters/conditions/is_attacking"] = false
+		animation_tree["parameters/conditions/is_idle"] = false
+	elif Input.is_action_pressed("left_click"):
+		animation_tree["parameters/conditions/is_attacking"] = true
+		animation_tree["parameters/conditions/is_blocking"] = false
+		animation_tree["parameters/conditions/is_idle"] = false
+	else:
+		animation_tree["parameters/conditions/is_idle"] = true
+		animation_tree["parameters/conditions/is_blocking"] = false
+		animation_tree["parameters/conditions/is_attacking"] = false
+	
 	
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	
 	if is_dashing:
 		velocity = velocity.normalized() * dash_speed
 	else:
-		velocity = input_direction * player_speed
+		velocity = input_direction * player_speed 
+		
 	
 	rotation = get_global_mouse_position().angle_to_point(position)
 	move_and_slide()
 	
 	if Input.is_action_just_pressed("space") and not is_dashing:
+		$Dash_timer.start()
 		is_dashing = true
-		dash_timer = dash_duration
-		player_speed = dash_speed  # ustaw prędkość gracza na prędkość dasha
+
+func _on_dash_timer_timeout():
+	player_speed = 400
+	is_dashing = false 
